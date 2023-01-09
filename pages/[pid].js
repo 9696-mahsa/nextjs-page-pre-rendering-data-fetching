eafabd4b57e56086fa48f3e2bd809984285ae973
Mahsa <mahsa.behmanesh74@gmail.com>
2023-01-09 11:47:24 +0330

    getStaticPaths

--- pages/[pid].js
+++ pages/[pid].js
@@ -6,6 +6,12 @@ export default function ProductDetailPage (props) {
 
     const { loadedProduct } = props;
 
+    if( !loadedProduct ) {   //for thi if() , fallback in getStaticPaths, must be true
+        return (
+            <p>Loading ....</p>
+        )
+    }
+
     return(
         <Fragment>
             <h1>{loadedProduct.title}</h1>
@@ -15,19 +21,42 @@ export default function ProductDetailPage (props) {
 
 }
 
+async function getData () {
+    const filePath = path.join(process.cwd(), 'data' , 'dummy-backend.json');
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData);
+    return data;
+}
+
 export async function getStaticProps(context) {
     const { params } = context;
     const productId = params.pid;
 
-    const filePath = path.join(process.cwd(), 'data' , 'dummy-backend.json');
-    const jsonData = await fs.readFile(filePath);
-    const data = JSON.parse(jsonData);
-
+    const data = await getData();
     const product = data.products.find(product => product.id === productId);
+
+    if( !product ) {
+        return {
+            notFound: true
+        }
+    }
 
     return {
         props: {
             loadedProduct : product
         }
     };
+}
+
+export async function getStaticPaths () {
+    const data = await getData();
+    const ids = data.products.map(product => product.id);
+    const pathsWithParams = ids.map((id) => ({params: { pid: id }}))
+    return {
+        paths: pathsWithParams,
+        fallback: true 
+        // false : pre-generating all the paths: {} ,
+        // true : just pre-generating only the path in paths:{} , and othe path will be gererated when they are needed
+        // 'blocking' : we dont need the fallback check in ProductDetailPage() -> if( !loadedProduct )
+    }
 }
